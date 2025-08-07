2f4b2f733c70013dc0674529ebecc34e92939472
hawkiee <gokulaudhayannv@gmail.com>
2025-08-07 13:00:56 +0530

    Install framer-motion

--- api/send.js
+++ api/send.js
@@ -6,13 +6,13 @@ export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Only POST requests allowed' });
   }
-
+  console.log(1);
   const { email, message } = req.body;
 
   if (!email || !message) {
     return res.status(400).json({ error: 'Email and message are required' });
   }
-
+  console.log(2, email, message);
   try {
     const transporter = nodemailer.createTransport({
       service: 'gmail',
@@ -21,16 +21,16 @@ export default async function handler(req, res) {
         pass: process.env.MY_EMAIL_PASSWORD,
       },
     });
-
+console.log(3, transporter);
     const mailOptions = {
       from: process.env.MY_EMAIL,
       to: process.env.MY_EMAIL,
       subject: `New Message from ${email}`,
       text: message,
     };
-
+console.log(4, mailOptions);
     await transporter.sendMail(mailOptions);
-
+console.log(5,'Email sent successfully');
     res.status(200).json({ success: true, message: 'Email sent successfully' });
   } catch (err) {
     console.error('Email sending error:', err);
